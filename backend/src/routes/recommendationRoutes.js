44cf3b04f398eca592f3f8326848604303c30d01
Aditya <adityautsav1901@gmail.com>
2025-09-13 23:27:32 +0530

    Remove all files and folders from repo

--- backend/src/routes/recommendationRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const ctrl = require("../controllers/recommendationController");
-
-router.get("/", ctrl.getAll);       // GET /api/recommendations
-router.get("/:id", ctrl.getById);   // GET /api/recommendations/:id
-
-module.exports = router;
